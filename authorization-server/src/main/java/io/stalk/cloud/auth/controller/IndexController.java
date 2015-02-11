1dd7603093a02bce5a2327c4c318edd1b78a7167
John Kim <yohany@gmail.com>
2015-02-11 18:39:21 +0900

    add menus

--- authorization-server/src/main/java/io/stalk/cloud/auth/controller/IndexController.java
+++ authorization-server/src/main/java/io/stalk/cloud/auth/controller/IndexController.java
@@ -1,74 +1,96 @@
 package io.stalk.cloud.auth.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.provider.approval.Approval;
-import org.springframework.security.oauth2.provider.approval.ApprovalStore;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
 import java.security.Principal;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static java.util.Arrays.asList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.approval.Approval;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
-    @Autowired
-    private JdbcClientDetailsService clientDetailsService;
+	@Autowired private JdbcClientDetailsService clientDetailsService;
+	@Autowired private ApprovalStore approvalStore;
+	@Autowired private TokenStore tokenStore;
 
-    @Autowired
-    private ApprovalStore approvalStore;
+	@RequestMapping("/")
+	public String root(Map<String, Object> model, Principal principal) {
+		return "redirect:/dashboard";
+	}
+	
+	@RequestMapping("/dashboard")
+	public ModelAndView dashboard(Map<String, Object> model, Principal principal) {
+		return new ModelAndView("dashboard", model);
+	}
 
-    @Autowired
-    private TokenStore tokenStore;
+	@RequestMapping("/approvals")
+	public ModelAndView approvals(Map<String, Object> model, Principal principal) {
+		List<Approval> approvals = clientDetailsService.listClientDetails().stream()
+				.map(clientDetail -> approvalStore.getApprovals(principal.getName(), clientDetail.getClientId()))
+				.flatMap(Collection::stream)
+				.collect(Collectors.toList());
+		model.put("approvals", approvals);
+		return new ModelAndView("approvals", model);
+	}
 
-    @RequestMapping("/")
-    public ModelAndView root(Map<String, Object> model, Principal principal) {
-        List<Approval> approvals = clientDetailsService.listClientDetails().stream()
-                .map(clientDetail -> approvalStore.getApprovals(principal.getName(), clientDetail.getClientId()))
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-        model.put("approvals", approvals);
-        model.put("clientDetails", clientDetailsService.listClientDetails());
-        return new ModelAndView("index", model);
+	@RequestMapping("/clients")
+	public ModelAndView clients(Map<String, Object> model, Principal principal) {
+		model.put("clientDetails", clientDetailsService.listClientDetails());
+		return new ModelAndView("clients", model);
+	}
+
+    @RequestMapping(value = "/clients/form", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('ROLE_OAUTH_ADMIN')")
+    public String showEditOrAddForm(@RequestParam(value = "client", required = false) String clientId, Model model) {
+        ClientDetails clientDetails;
+        if(clientId != null) {
+            clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        } else {
+            clientDetails = new BaseClientDetails();
+        }
+        model.addAttribute("clientDetails", clientDetails);
+        return "form";
     }
     
-    @RequestMapping("/approvals")
-    public ModelAndView approvals(Map<String, Object> model, Principal principal) {
-        List<Approval> approvals = clientDetailsService.listClientDetails().stream()
-                .map(clientDetail -> approvalStore.getApprovals(principal.getName(), clientDetail.getClientId()))
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
-        model.put("approvals", approvals);
-        return new ModelAndView("approvals", model);
+    @RequestMapping(value = "/clients/edit", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ROLE_OAUTH_ADMIN')")
+    public String editClient(
+            @ModelAttribute BaseClientDetails clientDetails,
+            @RequestParam(value = "newClient", required = false) String newClient
+            ) {
+        if (newClient == null) {
+            //does not update the secret!
+            clientDetailsService.updateClientDetails(clientDetails);
+        } else {
+            clientDetailsService.addClientDetails(clientDetails);
+        }
+
+        // If the user has entered a secret in the form update it.
+        if (!clientDetails.getClientSecret().isEmpty()) {
+            clientDetailsService.updateClientSecret(clientDetails.getClientId(), clientDetails.getClientSecret());
+        }
+        return "redirect:/clients";
     }
     
-    @RequestMapping("/clients")
-    public ModelAndView clients(Map<String, Object> model, Principal principal) {
-        model.put("clientDetails", clientDetailsService.listClientDetails());
-        return new ModelAndView("clients", model);
-    }
 
-    @RequestMapping(value = "/approval/revoke", method = RequestMethod.POST)
-    public String revokeApproval(@ModelAttribute Approval approval) {
-        approvalStore.revokeApprovals(asList(approval));
-        tokenStore
-                .findTokensByClientIdAndUserName(approval.getClientId(), approval.getUserId())
-                .forEach(tokenStore::removeAccessToken);
-        return "redirect:/approvals";
-    }
-
-    @RequestMapping("/login")
-    public String loginPage() {
-        return "login";
-    }
+	@RequestMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
 }
--- authorization-server/src/main/java/io/stalk/cloud/auth/controller/RestAPIController.java
+++ authorization-server/src/main/java/io/stalk/cloud/auth/controller/RestAPIController.java
@@ -1,13 +1,34 @@
 package io.stalk.cloud.auth.controller;
 
+import static java.util.Arrays.asList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.approval.Approval;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestAPIController {
 
+    @Autowired private ApprovalStore approvalStore;
+    @Autowired private TokenStore tokenStore;
+    
 	@RequestMapping("/ping")
 	public String ping() {
 		return "pong";
 	}
+	
+    @RequestMapping(value = "/approval/revoke", method = RequestMethod.POST)
+    public String revokeApproval(@ModelAttribute Approval approval) {
+        approvalStore.revokeApprovals(asList(approval));
+        tokenStore
+                .findTokensByClientIdAndUserName(approval.getClientId(), approval.getUserId())
+                .forEach(tokenStore::removeAccessToken);
+        return "redirect:/approvals";
+    }
+    
 }
